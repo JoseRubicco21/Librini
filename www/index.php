eea32b70604af9d649f4545753fa81c6b1c3b9f4
Jnv821 <Jnv821@gmail.com>
2024-10-17 20:46:36 +0200

    Inizio di progetto

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librini</title>
+</head>
+<body>
+    <h1>Benvenuto a Librini!</h1>
+</body>
+</html>
